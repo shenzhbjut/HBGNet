c5eca334c5dfc0d41ec9d672fd3c2da56c544fec
shenzhbjut <shenzh@emails.bjut.edu.cn>
2024-07-10 17:03:02 +0800

    Add files via upload

---
+++ utils/data/__init__.py
@@ -0,0 +1,18 @@
+def get_dataset(dataset_name):
+    if dataset_name == 'cornell':
+        from .cornell_data import CornellDataset
+        return CornellDataset
+    elif dataset_name == 'jacquard':
+        from .jacquard_data import JacquardDataset
+        return JacquardDataset
+    elif dataset_name == 'multi':
+        from .multi_object import CornellDataset
+        return CornellDataset
+    elif dataset_name == 'graspnet1b':
+        from .gn1b_data import GraspNet1BDataset
+        return GraspNet1BDataset
+    elif dataset_name == 'depth':
+        from .multi import MultiDataset
+        return MultiDataset
+    else:
+        raise NotImplementedError('Dataset Type {} is Not implemented'.format(dataset_name))
